97d1bf31d14c2b04e192b991fc6f74fa30a310c9
Lupo <bensender@gmail.com>
2025-11-17 19:23:03 +0200

    Preparing render deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = `${window.location.protocol}//${window.location.hostname}:8000/api`;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+  || `${window.location.protocol}//${window.location.hostname}:8000/api`;
 
 // ------------------------
 // EXPENSES
